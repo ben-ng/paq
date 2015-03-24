15dc1938f43fec7f8183a1d4a9b76c18fe2b8860
Ben Ng <me@benng.me>
2015-03-24 00:12:47 -0400

    Set up tests

---
+++ paq-tests/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  paq-tests
+//
+//  Created by Ben on 3/23/15.
+//  Copyright (c) 2015 Ben Ng. All rights reserved.
+//
+
+#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
+#include "catch.hpp"
+
+TEST_CASE( "Bundles Recursively", "[]" ) {
+    REQUIRE( 1 == 1 );
+}
