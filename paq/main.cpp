e3735e8ee18ba9f312072b96c858961ed2c3f619
Ben Ng <me@benng.me>
2015-03-23 23:51:45 -0400

    Add initial stuff

---
+++ paq/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  paq
+//
+//  Created by Ben on 3/23/15.
+//  Copyright (c) 2015 Ben Ng. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
